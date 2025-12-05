a8d4dfd0ad6480f5fbc721abab0a813b7d20ff83
adriahz <adri.hs.7@gmail.com>
2025-12-05 18:50:08 +0100

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
               href="#contact"
               className="px-4 py-2 bg-yellow-400 text-black rounded"
             >
-              Contacta
+              
             </a>
           </li>
         </ul>
