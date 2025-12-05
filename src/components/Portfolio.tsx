d4f9fbbcbf603ca097c1dc8acff751ca3db0a4c7
adriahz <adri.hs.7@gmail.com>
2025-12-05 19:44:48 +0100

    Updated Portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -7,7 +7,7 @@ export default function Portfolio() {
       image:
         'https://images.pexels.com/photos/433308/pexels-photo-433308.jpeg?auto=compress&cs=tinysrgb&w=800',
       title: 'Pavelló municipal - Mieres',
-      type: 'Comunitat de veïns',
+      type: 'Comunitat energètica',
       power: '100 kW',
       modules: '300',
       icon: Home,
